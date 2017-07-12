0277f85a893a62029c5c6d78761b79d806a5ceda
Kirill Sklyarov <kirill.sklyarov@gmail.com>
2017-07-12 21:51:20 +0300

    Multiple Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -71,7 +67,7 @@ const HEROES: Hero[] = [
   `],
   template: `
   <h1>{{title}}</h1>
-  
+
   <h2>My Heroes</h2>
   <ul class="heroes">
     <li *ngFor="let hero of heroes"
@@ -81,14 +77,7 @@ const HEROES: Hero[] = [
     </li>
   </ul>
 
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
+  <hero-detail [hero]="selectedHero"></hero-detail>
   `
 })
 export class AppComponent {
